776c8feb39f1cb1972b99a3c5d6b70083bf3433d
FagangJin <fagangjin@autox.ai>
2020-09-11 15:30:52 +0800

    first commit

---
+++ readme.md
@@ -0,0 +1,31 @@
+# NB
+
+Nenural network Blocks (aka: **NB**). This library provides massive fancy blocks for you for quick import to build your powerful. Some SOTA tricks and connections such as CSP, ASFF, Attention, BaseConv, Hardswish, all included for quick prototype your model.
+
+**nb** is an idea comes from engineering, we build model with some common blocks, we exploring new ideas with SOTA tricks, but all those thing can be gathered into one single place, and for model quick design and prototyping.
+
+this project is under construct for now, I will update it quickly once I found some new blocks that really works in model. Also, every single updated block will be recorded in updates.
+
+
+
+## Usage
+
+Here is an example of using NB to build YoloV5!
+
+```
+
+```
+
+
+
+
+
+## Updates
+
+- **2020.09.11**: new added blocks:
+
+  ```
+  CSPBottleneck
+  ```
+
+  
---
+++ setup.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2020 JinTian.
+#
+# This file is part of alfred
+# (see http://jinfagang.github.io).
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+"""
+install alfred into local bin dir.
+"""
+from setuptools import setup, find_packages
+from setuptools import setup, Extension
+import io
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with io.open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='alfred-py',
+      version='2.8.0',
+      keywords=['deep learning', 'script helper', 'tools'],
+      description='''
+      Alfred is a DeepLearning utility library.
+      ''',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      license='Apache 2.0',
+      packages=[
+          'alfred',
+          'alfred.dl',
+          'alfred.dl.inference',
+          'alfred.dl.data',
+          'alfred.dl.data.common',
+          'alfred.dl.data.meta',
+          'alfred.dl.torch',
+          'alfred.dl.torch.train',
+          'alfred.dl.torch.distribute',
+          'alfred.dl.torch.runner',
+          'alfred.dl.torch.nn',
+          'alfred.dl.torch.nn.modules',
+          'alfred.dl.torch.ops',
+          'alfred.dl.tf',
+          'alfred.vis',
+          'alfred.modules',
+          'alfred.modules.scrap',
+          'alfred.modules.text',
+          'alfred.modules.vision',
+          'alfred.modules.data',
+          'alfred.modules.cabinet',
+          'alfred.modules',
+          'alfred.fusion',
+          'alfred.vis.image',
+          'alfred.vis.pointcloud',
+          'alfred.utils',
+          'alfred.protos'
+      ],
+      # package_dir={'alfred': 'alfred'},
+      entry_points={
+          'console_scripts': [
+              'alfred = alfred.alfred:main'
+          ]
+      },
+      include_package_data=True,
+      author="Lucas Jin",
+      author_email="jinfagang19@163.com",
+      url='https://github.com/jinfagang/alfred',
+      platforms='any',
+      install_requires=['colorama', 'requests', 'regex',
+                        'future', 'deprecated', 'loguru', 'pyquaternion', 'lxml']
+      )
