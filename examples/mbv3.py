af251eb47cd0fd7ded7f1a8bdffd4bd3b52fda17
FagangJin <fagangjin@autox.ai>
2020-09-21 14:09:00 +0800

    add mobilenetv3

---
+++ examples/mbv3.py
@@ -0,0 +1,27 @@
+"""
+
+Test mobilenetv3 based on nb
+"""
+
+from nb.torch.backbones.mobilenetv3 import mobilenetv3_large
+
+import torch
+
+
+
+if __name__ == "__main__":
+    a = torch.randn([1, 3, 512, 512])
+    
+    m = mobilenetv3_large(fpn_levels=[5, 9, 14])
+    
+    o = m(a)
+    print(o)
+    for i in o:
+        print(i.shape)
+
+    m = mobilenetv3_large(num_classes=8)
+    
+    o = m(a)
+    print(o)
+    for i in o:
+        print(i.shape)
